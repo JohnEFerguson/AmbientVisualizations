5e6f9f1209508653a79f051efec5674d0bd6be6e
Jack Ferguson <jef1@williams.edu>
2018-05-19 10:14:02 -0400

    added everything

---
+++ Spring-2018/scripts/all/paths.js
@@ -0,0 +1,7 @@
+PROJ_DIR = "~/public_html/AmbientVisualizations/Spring-2018/";
+KITCHEN_IMAGES_PATH = "../../images/kitchen/";
+CIRCLES_IMAGES_PATH = PROJ_DIR + "images/circles/";
+LABELED_CIRCLES_IMAGES_PATH = PROJ_DIR + "images/labeled_circles/";
+PINWHEELS_IMAGES_PATH = PROJ_DIR + "images/pinwheels/";
+HEXAGONS_IMAGES_PATH = PROJ_DIR + "images/hexagons/";
+SCRIPTS_PATH = PROJ_DIR + "scripts/";
